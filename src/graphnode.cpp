f19d83d50ad391e868c369618a2b8289d8843a10
David Nunez <arizonatribe@gmail.com>
2023-03-17 21:22:12 -0600

    Modified the AddEdgeToChildNode to expect a unique_ptr instance to be moved into it

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -6,6 +6,8 @@ GraphNode::GraphNode(int id)
     _id = id;
 }
 
+GraphNode::~GraphNode() { }
+
 void GraphNode::AddToken(std::string token)
 {
     _answers.push_back(token);
@@ -16,9 +18,9 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 {
-    _childEdges.push_back(std::unique_ptr<GraphEdge>(edge));
+    _childEdges.push_back(std::move(edge));
 }
 
 //// STUDENT CODE
